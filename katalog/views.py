f54ee67fb21beaff95c7b5b2865336c23bca6282
unknown <ahmad.hanif11@ui.ac..id>
2022-09-14 10:37:15 +0700

    Routing and data mapping to katalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
+from multiprocessing import context
 from django.shortcuts import render
+from katalog.models import CatalogItem 
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_barang = CatalogItem.objects.all()
+    context = {
+        "list_barang": data_barang,
+        "nama": "Ahmad Hanif",
+        "npm": "2106750603"
+    }
+    return render(request, "katalog.html", context)
