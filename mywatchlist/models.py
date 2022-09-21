3fe7eb06cf32a8cd27921f4334e10df1f1760d65
Ahmad Hanif <ahmad.hanif11@ui.ac.id>
2022-09-22 01:18:38 +0700

    Assignment 3

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,7 +1,7 @@
 from django.db import models
  
 class MyWatchList(models.Model):
-    watched = models.CharField(max_length=6)
+    watched = models.CharField(max_length=10)
     title = models.CharField(max_length=255)
     rating = models.IntegerField()
     release_date = models.DateField()
